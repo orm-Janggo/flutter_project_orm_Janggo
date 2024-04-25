b401642b4bc73623730fc0ffc1a79941fc445597
leepl <yplunap25@gmail.com>
2024-04-25 17:54:31 +0900

    [ADD] picture data source

---
+++ lib/data/data_source/picture_data_source.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../dto/picture_dto.dart';
+
+class PictureDataSource {
+  final _baseUrl = 'https://pixabay.com/api/?key=';
+  final _key = '43565705-c9895710d0a4c1f822464b641';
+
+  Future<PictureDto> getPictureData(String query) async {
+    final response = await http
+        .get(Uri.parse('$_baseUrl$_key&q=$query&image_type=photo&pretty=true'));
+
+    return PictureDto.fromJson(jsonDecode(response.body));
+  }
+}
