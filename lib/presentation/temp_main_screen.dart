39db0744bd56f3fd7b5f4bea7cb32778c6215c71
kimhyungmin <jeen14rla@gmail.com>
2024-04-26 18:19:17 +0900

    [CHORE] add import

--- lib/presentation/temp_main_screen.dart
+++ lib/presentation/temp_main_screen.dart
@@ -1,12 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_project_orm_janggo/authentication/authentication.dart';
+import 'package:flutter_project_orm_janggo/presentation/sign/sign_state.dart';
+import 'package:provider/provider.dart';
 
 class TempMainScreen extends StatelessWidget {
   const TempMainScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-
+    return Consumer<SignState>(
+      builder: (context, appState, _) => AuthFunc(
+          loggedIn: appState.loggedIn,
+          signOut: () {
+            FirebaseAuth.instance.signOut();
+          }),
     );
   }
 }
