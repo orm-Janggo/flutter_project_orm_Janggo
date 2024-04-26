bd350d5c3b8a7d0f352b28a55f4292fb04bb3086
kimhyungmin <jeen14rla@gmail.com>
2024-04-26 21:43:43 +0900

    [CHORE] modify import auth

--- lib/presentation/temp_main_screen.dart
+++ lib/presentation/temp_main_screen.dart
@@ -1,6 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_project_orm_janggo/authentication/authentication.dart';
+import 'package:flutter_project_orm_janggo/presentation/authentication/authentication.dart';
 import 'package:flutter_project_orm_janggo/presentation/sign/sign_state.dart';
 import 'package:provider/provider.dart';
 
