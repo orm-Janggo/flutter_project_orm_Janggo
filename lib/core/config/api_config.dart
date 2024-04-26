a039a48f91c61e24eacef3171feb4000b391e506
hee462 <a01080809887@gmail.com>
2024-04-26 18:35:06 +0900

    [ADD] Kakao login key 값 생성

--- lib/core/config/api_config.dart
+++ lib/core/config/api_config.dart
@@ -9,3 +9,8 @@ class PixabayConfig {
   static final String baseUrl = dotenv.env['PIXABAY_BASE_URL'].toString();
   static final String apiKey = dotenv.env['PIXABAY_API_KEY'].toString();
 }
+
+class KakaoConfig {
+  static final String nativeAppKey = dotenv.env['KAKAO_NATIVE_APP_KEY'].toString();
+  static final String javascriptKey = dotenv.env['KAKAO_JAVASCRIPT_KEY'].toString();
+}
