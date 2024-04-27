d9dbd61efef74e37e794155ddfcc953ec8549423
hee462 <a01080809887@gmail.com>
2024-04-27 14:23:53 +0900

    [ADD] go_router 주입

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
