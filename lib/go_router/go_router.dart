4f7004be339775ca38606ab929c0bb90ab9bd503
kimhyungmin <jeen14rla@gmail.com>
2024-04-28 15:25:13 +0900

    [MODIFY] change sign in screen

--- lib/go_router/go_router.dart
+++ lib/go_router/go_router.dart
@@ -1,8 +1,7 @@
-import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_project_orm_janggo/presentation/login_screen.dart';
 import 'package:flutter_project_orm_janggo/presentation/main_screen.dart';
-import 'package:flutter_project_orm_janggo/presentation/temp_main_screen.dart';
+import 'package:flutter_project_orm_janggo/presentation/sign/sign_in/sign_in_screen.dart';
 import 'package:go_router/go_router.dart';
 
 import '../presentation/recipe_screen.dart';
@@ -16,65 +15,41 @@ final router = GoRouter(
         GoRoute(
           path: 'sign-in',
           builder: (context, state) {
-            return SignInScreen(
-              actions: [
-                ForgotPasswordAction(((context, email) {
-                  final uri = Uri(
-                    path: '/sign-in/forgot-password',
-                    queryParameters: <String, String?>{
-                      'email': email,
-                    },
-                  );
-                  context.push(uri.toString());
-                })),
-                AuthStateChangeAction(((context, state) {
-                  final user = switch (state) {
-                    SignedIn state => state.user,
-                    UserCreated state => state.credential.user,
-                    _ => null
-                  };
-                  if (user == null) {
-                    return;
-                  }
-                  if (state is UserCreated) {
-                    user.updateDisplayName(user.email!.split('@')[0]);
-                  }
-                  if (!user.emailVerified) {
-                    user.sendEmailVerification();
-                    const snackBar = SnackBar(
-                        content: Text(
-                            'Please check your email to verify your email address'));
-                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                  }
-                  context.pushReplacement('/');
-                })),
-              ],
-            );
-          },
-          routes: [
-            GoRoute(
-              path: 'forgot-password',
-              builder: (context, state) {
-                final arguments = state.uri.queryParameters;
-                return ForgotPasswordScreen(
-                  email: arguments['email'],
-                  headerMaxExtent: 200,
-                );
-              },
-            ),
-          ],
-        ),
-        GoRoute(
-          path: 'profile',
-          builder: (context, state) {
-            return ProfileScreen(
-              providers: const [],
-              actions: [
-                SignedOutAction((context) {
-                  context.pushReplacement('/');
-                }),
-              ],
-            );
+            return SignInScreen();
+            // return SignInScreen(
+            //   actions: [
+            //     ForgotPasswordAction(((context, email) {
+            //       final uri = Uri(
+            //         path: '/sign-in/forgot-password',
+            //         queryParameters: <String, String?>{
+            //           'email': email,
+            //         },
+            //       );
+            //       context.push(uri.toString());
+            //     })),
+            //     AuthStateChangeAction(((context, state) {
+            //       final user = switch (state) {
+            //         SignedIn state => state.user,
+            //         UserCreated state => state.credential.user,
+            //         _ => null
+            //       };
+            //       if (user == null) {
+            //         return;
+            //       }
+            //       if (state is UserCreated) {
+            //         user.updateDisplayName(user.email!.split('@')[0]);
+            //       }
+            //       if (!user.emailVerified) {
+            //         user.sendEmailVerification();
+            //         const snackBar = SnackBar(
+            //             content: Text(
+            //                 'Please check your email to verify your email address'));
+            //         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+            //       }
+            //       context.pushReplacement('/');
+            //     })),
+            //   ],
+            // );
           },
         ),
         GoRoute(
