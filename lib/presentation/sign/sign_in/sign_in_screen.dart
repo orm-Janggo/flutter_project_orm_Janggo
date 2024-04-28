1b258fc49b257466e8cbe52c6ebe7535d3a698a2
kimhyungmin <jeen14rla@gmail.com>
2024-04-28 15:25:30 +0900

    [ADD] add sign in screen

---
+++ lib/presentation/sign/sign_in/sign_in_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatelessWidget {
+  const SignInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: 24,
+          ),
+          Text('Hi, Welcome Back 🙌'),
+          Text('Hello again, you\'ve been missed!'),
+          Form(
+              child: Column(
+            children: [
+              TextFormField(),
+              TextFormField(),
+            ],
+          )),
+        ],
+      ),
+    );
+  }
+}
