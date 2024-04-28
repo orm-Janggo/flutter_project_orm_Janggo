4f0bc278a207d4f4a4ebd9f76f54e7b59c9439f7
jiwon Choi <jiwon31273@naver.com>
2024-04-28 16:35:41 +0900

    [ADD] recipe text success

--- lib/domain/use_case/get_picture_use_case.dart
+++ lib/domain/use_case/get_picture_use_case.dart
@@ -1,12 +1,16 @@
+import 'package:flutter_project_orm_janggo/data/repository/picture_repository_impl.dart';
 import 'package:flutter_project_orm_janggo/domain/model/picture_model.dart';
 import 'package:flutter_project_orm_janggo/domain/repository/picture_repository.dart';
 
 class GetPictureUseCase {
-  final PictureRepository _repository;
+  final PictureRepositoryImpl _repository;
 
-  GetPictureUseCase(this._repository);
+  const GetPictureUseCase({
+    required PictureRepositoryImpl repository,
+  }) : _repository = repository;
 
   Future<PictureModel> execute(String query) async {
     return (await _repository.getPicture(query))[0];
   }
+
 }
---
+++ lib/presentation/recipe_state.freezed.dart
@@ -0,0 +1,152 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'recipe_state.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+RecipeState _$RecipeStateFromJson(Map<String, dynamic> json) {
+  return _RecipeState.fromJson(json);
+}
+
+/// @nodoc
+mixin _$RecipeState {
+  String get recipe => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $RecipeStateCopyWith<RecipeState> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $RecipeStateCopyWith<$Res> {
+  factory $RecipeStateCopyWith(
+          RecipeState value, $Res Function(RecipeState) then) =
+      _$RecipeStateCopyWithImpl<$Res, RecipeState>;
+  @useResult
+  $Res call({String recipe});
+}
+
+/// @nodoc
+class _$RecipeStateCopyWithImpl<$Res, $Val extends RecipeState>
+    implements $RecipeStateCopyWith<$Res> {
+  _$RecipeStateCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? recipe = null,
+  }) {
+    return _then(_value.copyWith(
+      recipe: null == recipe
+          ? _value.recipe
+          : recipe // ignore: cast_nullable_to_non_nullable
+              as String,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$RecipeStateImplCopyWith<$Res>
+    implements $RecipeStateCopyWith<$Res> {
+  factory _$$RecipeStateImplCopyWith(
+          _$RecipeStateImpl value, $Res Function(_$RecipeStateImpl) then) =
+      __$$RecipeStateImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call({String recipe});
+}
+
+/// @nodoc
+class __$$RecipeStateImplCopyWithImpl<$Res>
+    extends _$RecipeStateCopyWithImpl<$Res, _$RecipeStateImpl>
+    implements _$$RecipeStateImplCopyWith<$Res> {
+  __$$RecipeStateImplCopyWithImpl(
+      _$RecipeStateImpl _value, $Res Function(_$RecipeStateImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? recipe = null,
+  }) {
+    return _then(_$RecipeStateImpl(
+      recipe: null == recipe
+          ? _value.recipe
+          : recipe // ignore: cast_nullable_to_non_nullable
+              as String,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$RecipeStateImpl implements _RecipeState {
+  const _$RecipeStateImpl({this.recipe = ''});
+
+  factory _$RecipeStateImpl.fromJson(Map<String, dynamic> json) =>
+      _$$RecipeStateImplFromJson(json);
+
+  @override
+  @JsonKey()
+  final String recipe;
+
+  @override
+  String toString() {
+    return 'RecipeState(recipe: $recipe)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$RecipeStateImpl &&
+            (identical(other.recipe, recipe) || other.recipe == recipe));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode => Object.hash(runtimeType, recipe);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$RecipeStateImplCopyWith<_$RecipeStateImpl> get copyWith =>
+      __$$RecipeStateImplCopyWithImpl<_$RecipeStateImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$RecipeStateImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _RecipeState implements RecipeState {
+  const factory _RecipeState({final String recipe}) = _$RecipeStateImpl;
+
+  factory _RecipeState.fromJson(Map<String, dynamic> json) =
+      _$RecipeStateImpl.fromJson;
+
+  @override
+  String get recipe;
+  @override
+  @JsonKey(ignore: true)
+  _$$RecipeStateImplCopyWith<_$RecipeStateImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
