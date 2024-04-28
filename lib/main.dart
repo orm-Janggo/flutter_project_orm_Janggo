869d2be0c1927d864ff9c3b0c70cca1557d5eb3e
hee462 <a01080809887@gmail.com>
2024-04-28 15:17:14 +0900

    [MODIFY] 테마 컬러 ,로직변경

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
         useMaterial3: true,
       ),
       routerConfig: router,
