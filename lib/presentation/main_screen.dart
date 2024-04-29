826b95d0a852783b6ff02459f1de21dbe89255cf
kimhyungmin <jeen14rla@gmail.com>
2024-04-29 22:58:32 +0900

    [STYLE] align code

--- lib/presentation/main_screen.dart
+++ lib/presentation/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project_orm_janggo/data/repository/chat_gpt_reopository_impl.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart'; // go_router 임포트 추가
 
@@ -8,7 +7,7 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<StatefulWidget> {
- User? _user;
+  User? _user;
 
   @override
   void initState() {
@@ -21,7 +20,9 @@ class _MainScreenState extends State<StatefulWidget> {
     setState(() {});
   }
 
-  final List<TextEditingController> _controllers = [TextEditingController()]; // 재료 텍스트 필드 컨트롤러
+  final List<TextEditingController> _controllers = [
+    TextEditingController()
+  ]; // 재료 텍스트 필드 컨트롤러
 
   void _addTextField() {
     // 재료 추가
@@ -89,11 +90,10 @@ class _MainScreenState extends State<StatefulWidget> {
         children: [
           Container(
             decoration: const BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/main_background.png'),
-                fit: BoxFit.cover,
-              )
-            ),
+                image: DecorationImage(
+              image: AssetImage('assets/images/main_background.png'),
+              fit: BoxFit.cover,
+            )),
           ),
           SingleChildScrollView(
             child: Column(
@@ -111,25 +111,26 @@ class _MainScreenState extends State<StatefulWidget> {
                           },
                         ),
                         labelText: '재료 ${i + 1}',
-                        filled: true,  // 배경 채우기 활성화
-                        fillColor: Colors.white,  // 입력란 배경 색깔
+                        filled: true,
+                        // 배경 채우기 활성화
+                        fillColor: Colors.white,
+                        // 입력란 배경 색깔
                         border: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.green,  // 기본 테두리 색깔
-                            width: 3.0
-                          ),
+                              color: Colors.green, // 기본 테두리 색깔
+                              width: 3.0),
                         ),
                         focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.blue,  // 활성화된 테두리 색깔
-                            width: 4.0,  // 테두리 두께
+                            color: Colors.blue, // 활성화된 테두리 색깔
+                            width: 4.0, // 테두리 두께
                           ),
                           borderRadius: BorderRadius.circular(12.0),
                         ),
                         // 비활성화된 테두리 색깔 변경
                         enabledBorder: OutlineInputBorder(
                           borderSide: BorderSide(
-                            color: Colors.white70,  // 비활성화된 테두리 색깔
+                            color: Colors.white70, // 비활성화된 테두리 색깔
                             width: 3.0,
                           ),
                           borderRadius: BorderRadius.circular(12.0),
@@ -156,20 +157,23 @@ class _MainScreenState extends State<StatefulWidget> {
                   color: Colors.orange,
                 ),
                 child: TextButton(
-                  onPressed: () => _onButtonPressed(context),  // 버튼 클릭 시 함수 호출
-                  child: Row(  // 텍스트와 아이콘을 나란히 배치하기 위해 Row 사용
-                    mainAxisSize: MainAxisSize.min,  // Row의 크기를 자식 위젯에 맞추기
+                  onPressed: () => _onButtonPressed(context), // 버튼 클릭 시 함수 호출
+                  child: Row(
+                    // 텍스트와 아이콘을 나란히 배치하기 위해 Row 사용
+                    mainAxisSize: MainAxisSize.min, // Row의 크기를 자식 위젯에 맞추기
                     children: [
                       Text(
-                        'Next',  // 텍스트 부분
+                        'Next', // 텍스트 부분
                         style: TextStyle(
                           fontSize: 16,
                           fontWeight: FontWeight.bold,
                           color: Colors.black,
                         ),
                       ),
-                      SizedBox(width: 8),  // 텍스트와 아이콘 사이에 간격 두기
-                      Icon(Icons.arrow_forward, color: Colors.black),  // 원하는 아이콘 추가
+                      SizedBox(width: 8),
+                      // 텍스트와 아이콘 사이에 간격 두기
+                      Icon(Icons.arrow_forward, color: Colors.black),
+                      // 원하는 아이콘 추가
                     ],
                   ),
                 ),
