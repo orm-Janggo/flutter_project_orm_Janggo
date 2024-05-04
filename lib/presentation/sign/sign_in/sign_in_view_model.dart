f791f35a3d9d280a7bf50402f5bea6e3818b6a12
kimhyungmin <jeen14rla@gmail.com>
2024-05-04 17:12:54 +0900

    [COMMENT] comment code need to add in sign in view model

--- lib/presentation/sign/sign_in/sign_in_view_model.dart
+++ lib/presentation/sign/sign_in/sign_in_view_model.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
 class SignInViewModel with ChangeNotifier {
+  // final SignInRepository _signInRepository;
+  // SignInState _signInState = const SignInState();
+  // SignInState get signInState => _signInState;
 
+  // void signInWithFirebaseAuth(String inputEmail, String inputPassword) {
+  //
+  // }
 }
