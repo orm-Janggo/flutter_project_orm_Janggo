294c7c34091e8e21877feb4150653b7fccee5232
kimhyungmin <jeen14rla@gmail.com>
2024-05-04 17:37:09 +0900

    [COMMENT] comment code need to add in forgot password view model

--- lib/presentation/sign/forgot_password/forgot_password_view_model.dart
+++ lib/presentation/sign/forgot_password/forgot_password_view_model.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
 class ForgotPasswordViewModel with ChangeNotifier {
+  // final ForgotPasswordRepository = _forgotPasswordRepository;
 
+  // void passwordResetWithFirebaseAuth(String inputEmail) {
+  //   sendPasswordResetEmail
+  // }
 }
