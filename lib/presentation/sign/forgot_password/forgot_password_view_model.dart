d7a7789180810d708465e6555e7200ac445f15ac
kimhyungmin <jeen14rla@gmail.com>
2024-05-04 17:45:21 +0900

    [CHORE] change import cupertino to material

--- lib/presentation/sign/forgot_password/forgot_password_view_model.dart
+++ lib/presentation/sign/forgot_password/forgot_password_view_model.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ForgotPasswordViewModel with ChangeNotifier {
   // final ForgotPasswordRepository = _forgotPasswordRepository;
