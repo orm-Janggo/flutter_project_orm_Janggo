28b65b23c7d85c05c7b73d871e6f4015cf6e356a
kimhyungmin <jeen14rla@gmail.com>
2024-05-05 19:14:46 +0900

    [ADD] add sign up view model

--- lib/presentation/sign/sign_up/sign_up_view_model.dart
+++ lib/presentation/sign/sign_up/sign_up_view_model.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/sign_up_with_email_password_use_case.dart';
 
 class SignUpViewModel with ChangeNotifier {
-  // final SignInRepository _signInRepository;
-  // SignUpState _signUpState = const SignUpState();
-  // SignUpState get signUpState => _signUpState;
+  final SignUpWithEmailPasswordUseCase _signUpWithEmailPasswordUseCase;
 
-  // void signUpWithFirebaseAuth(String inputEmail, String inputPassword) {
-  //   createUserWithEmailAndPassword
-  // }
+  SignUpViewModel(
+      {required SignUpWithEmailPasswordUseCase signUpWithEmailPasswordUseCase})
+      : _signUpWithEmailPasswordUseCase = signUpWithEmailPasswordUseCase;
+
+  Future<void> signUpWithFirebaseAuth(
+      String inputEmail, String inputPassword) async {
+    await _signUpWithEmailPasswordUseCase.execute(inputEmail, inputPassword);
+
+    notifyListeners();
+  }
 }
