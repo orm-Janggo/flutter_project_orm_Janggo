b85e52e0f82e78a1395fec45c05bcfb67153b32a
kimhyungmin <jeen14rla@gmail.com>
2024-05-05 12:09:19 +0900

    [ADD] add user info model by freezed and build runner rebuild

---
+++ lib/domain/model/user_info_model/user_info_model.g.dart
@@ -0,0 +1,21 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_info_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$UserInfoModelImpl _$$UserInfoModelImplFromJson(Map<String, dynamic> json) =>
+    _$UserInfoModelImpl(
+      displayName: json['displayName'] as String,
+      email: json['email'] as String,
+      uid: json['uid'] as String,
+    );
+
+Map<String, dynamic> _$$UserInfoModelImplToJson(_$UserInfoModelImpl instance) =>
+    <String, dynamic>{
+      'displayName': instance.displayName,
+      'email': instance.email,
+      'uid': instance.uid,
+    };
