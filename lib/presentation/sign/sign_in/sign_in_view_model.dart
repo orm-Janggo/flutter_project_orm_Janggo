68cf92f2c1757d4c2a343137eb42eaeda469cd56
kimhyungmin <jeen14rla@gmail.com>
2024-05-05 15:52:54 +0900

    [ADD] modify sign in view model constructor

--- lib/presentation/sign/sign_in/sign_in_view_model.dart
+++ lib/presentation/sign/sign_in/sign_in_view_model.dart
@@ -4,7 +4,9 @@ import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_cas
 class SignInViewModel with ChangeNotifier {
   final SignInWithEmailPasswordUseCase _signInWithEmailPasswordUseCase;
 
-  SignInViewModel(this._signInWithEmailPasswordUseCase);
+  SignInViewModel(
+      {required SignInWithEmailPasswordUseCase signInWithEmailPasswordUseCase})
+      : _signInWithEmailPasswordUseCase = signInWithEmailPasswordUseCase;
 
   void signInWithFirebaseAuth(String inputEmail, String inputPassword) async {
     await _signInWithEmailPasswordUseCase.execute(inputEmail, inputPassword);
