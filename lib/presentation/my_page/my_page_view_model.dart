5adc3cc8525de7842ed4bedfe41b33c1cfa3787e
kimhyungmin <jeen14rla@gmail.com>
2024-05-06 19:47:45 +0900

    [ADD] add functions to my page view model

--- lib/presentation/my_page/my_page_view_model.dart
+++ lib/presentation/my_page/my_page_view_model.dart
@@ -1,25 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_orm_janggo/domain/model/user_info_model/user_info_model.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/auth_state_changes_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_display_name_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_password_use_case.dart';
 
 class MyPageViewModel with ChangeNotifier {
   final AuthStateChangesUseCase _authStateChangesUseCase;
+  final UpdateDisplayNameUseCase _updateDisplayNameUseCase;
+  final UpdatePasswordUseCase _updatePasswordUseCase;
 
-  MyPageViewModel({required AuthStateChangesUseCase authStateChangesUseCase})
-      : _authStateChangesUseCase = authStateChangesUseCase;
+  MyPageViewModel({
+    required AuthStateChangesUseCase authStateChangesUseCase,
+    required UpdateDisplayNameUseCase updateDisplayNameUseCase,
+    required UpdatePasswordUseCase updatePasswordUseCase,
+  })  : _authStateChangesUseCase = authStateChangesUseCase,
+        _updateDisplayNameUseCase = updateDisplayNameUseCase,
+        _updatePasswordUseCase = updatePasswordUseCase;
 
-  String? userEmail;
-  String? userDisplayName;
-  UserInfoModel? firebaseUser;
+  String? _userEmail;
+  String? _userDisplayName;
+  UserInfoModel? _firebaseUser;
+
+  String? get userEmail => _userEmail;
+
+  String? get userDisplayName => _userDisplayName;
+
+  UserInfoModel? get firebaseUser => _firebaseUser;
 
   void getCurrentUserInfo() {
-    firebaseUser = _authStateChangesUseCase.execute();
+    _firebaseUser = _authStateChangesUseCase.execute();
     notifyListeners();
 
-    userEmail = firebaseUser?.email;
+    _userEmail = firebaseUser?.email;
     notifyListeners();
 
-    userDisplayName = firebaseUser?.displayName;
+    _userDisplayName = firebaseUser?.displayName;
+    notifyListeners();
+  }
+
+  void updateCurrentUserDisplayName(String inputDisplayName) async {
+    _userDisplayName = inputDisplayName;
+    await _updateDisplayNameUseCase.execute(inputDisplayName);
+
+    notifyListeners();
+  }
+
+  void updateCurrentUserPassword(String inputPassword) async {
+    await _updatePasswordUseCase.execute(inputPassword);
+
     notifyListeners();
   }
 }
