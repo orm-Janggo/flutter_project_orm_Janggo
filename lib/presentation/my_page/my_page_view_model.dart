a2ab74d60723f7852d33984442e9bee21ec1d1a8
kimhyungmin <jeen14rla@gmail.com>
2024-05-06 21:48:10 +0900

    [ADD] add sign out use case to view model

--- lib/presentation/my_page/my_page_view_model.dart
+++ lib/presentation/my_page/my_page_view_model.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project_orm_janggo/domain/model/user_info_model/user_info_model.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/auth_state_changes_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/sign_out_use_case.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_display_name_use_case.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_password_use_case.dart';
 
@@ -8,14 +9,17 @@ class MyPageViewModel with ChangeNotifier {
   final AuthStateChangesUseCase _authStateChangesUseCase;
   final UpdateDisplayNameUseCase _updateDisplayNameUseCase;
   final UpdatePasswordUseCase _updatePasswordUseCase;
+  final SignOutUseCase _signOutUseCase;
 
   MyPageViewModel({
     required AuthStateChangesUseCase authStateChangesUseCase,
     required UpdateDisplayNameUseCase updateDisplayNameUseCase,
     required UpdatePasswordUseCase updatePasswordUseCase,
+    required SignOutUseCase signOutUseCase,
   })  : _authStateChangesUseCase = authStateChangesUseCase,
         _updateDisplayNameUseCase = updateDisplayNameUseCase,
-        _updatePasswordUseCase = updatePasswordUseCase;
+        _updatePasswordUseCase = updatePasswordUseCase,
+        _signOutUseCase = signOutUseCase;
 
   String? _userEmail;
   String? _userDisplayName;
@@ -47,6 +51,12 @@ class MyPageViewModel with ChangeNotifier {
 
   void updateCurrentUserPassword(String inputPassword) async {
     await _updatePasswordUseCase.execute(inputPassword);
+
+    notifyListeners();
+  }
+
+  void signOutCurrentUser() async {
+    await _signOutUseCase.execute();
 
     notifyListeners();
   }
