5847e586d057eb8e08b59661018fef9e48f9115e
kimhyungmin <jeen14rla@gmail.com>
2024-05-06 22:10:00 +0900

    [ADD] add main screen view model

---
+++ lib/presentation/main_screen/main_screen_view_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_project_orm_janggo/domain/model/user_info_model/user_info_model.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/auth_state_changes_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/sign_out_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_display_name_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/update_password_use_case.dart';
+
+class MainScreenViewModel with ChangeNotifier {
+  final AuthStateChangesUseCase _authStateChangesUseCase;
+
+  MainScreenViewModel({
+    required AuthStateChangesUseCase authStateChangesUseCase,
+  }) : _authStateChangesUseCase = authStateChangesUseCase;
+
+  String? _userEmail;
+  String? _userDisplayName;
+  UserInfoModel? _firebaseUser;
+
+  String? get userEmail => _userEmail;
+
+  String? get userDisplayName => _userDisplayName;
+
+  UserInfoModel? get firebaseUser => _firebaseUser;
+
+  void getCurrentUserInfo() {
+    _firebaseUser = _authStateChangesUseCase.execute();
+    notifyListeners();
+
+    _userEmail = firebaseUser?.email;
+    notifyListeners();
+
+    _userDisplayName = firebaseUser?.displayName;
+    notifyListeners();
+  }
+}
