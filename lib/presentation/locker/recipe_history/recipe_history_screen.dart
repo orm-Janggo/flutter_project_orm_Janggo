a336cea9de06f1e43b9f7b95516ff223c8e71215
leepl <yplunap25@gmail.com>
2024-05-06 14:48:59 +0900

    [CHORE] history list tile border 변경

--- lib/presentation/locker/recipe_history/recipe_history_screen.dart
+++ lib/presentation/locker/recipe_history/recipe_history_screen.dart
@@ -13,16 +13,17 @@ class _RecipeHistoryScreenState extends State<RecipeHistoryScreen> {
   @override
   Widget build(BuildContext context) {
     // 가로화면 사이즈
-    num respWidth = MediaQuery.of(context).size.width;
+    final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xffFB8C00),
+        backgroundColor: const Color(0xffFB8C00),
       ),
       body: Container(
-        padding: EdgeInsets.symmetric(horizontal: respWidth * 0.05),
-        color: Color(0xffFB8C00),
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+        color: const Color(0xffFB8C00),
         child: ListView.builder(
+          itemCount: 10,
           itemBuilder: (context, index) {
             return HistoryRecipeListTile(context, index);
           },
@@ -39,24 +40,28 @@ Widget HistoryRecipeListTile(context, int index) {
   return Container(
     margin: EdgeInsets.symmetric(vertical: screenHeight * 0.01),
     padding: EdgeInsets.all(screenWidth * 0.03),
-    color: Colors.white,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(8.0)
+    ),
+
     child: Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Container(
           width: screenWidth * 0.3,
-          child: Image.asset('assets/images/empty_image.png'),
+          child: ClipRRect(borderRadius: BorderRadius.circular(8.0), child: Image.asset('assets/images/empty_image.png')),
         ),
-        Text('레시피 이름'),
+        Text('레시피 이름 $index'),
         Column(
           children: [
             IconButton(
               onPressed: () {},
-              icon: Icon(Icons.delete_outline),
+              icon: const Icon(Icons.delete_outline),
             ),
             IconButton(
               onPressed: () {},
-              icon: Icon(Icons.favorite_border_outlined),
+              icon: const Icon(Icons.favorite_border_outlined),
             ),
           ],
         ),
