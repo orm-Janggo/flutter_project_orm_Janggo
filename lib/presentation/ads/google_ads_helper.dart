63daa2851efb4ac268cf996dfe05d6ff5aa50f14
kimhyungmin <jeen14rla@gmail.com>
2024-05-08 15:57:50 +0900

    [MODIFY] change unit id to test unit id

--- lib/presentation/ads/google_ads_helper.dart
+++ lib/presentation/ads/google_ads_helper.dart
@@ -1,34 +1,34 @@
 import 'dart:io';
 
-class GoogleAdsHelper {
+class AdHelper {
 
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return '<YOUR_ANDROID_BANNER_AD_UNIT_ID>';
+      return 'ca-app-pub-3940256099942544/6300978111';
     } else if (Platform.isIOS) {
-      return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
+      return 'ca-app-pub-3940256099942544/2934735716';
     } else {
-      throw UnsupportedError('Unsupported platform');
+      throw new UnsupportedError('Unsupported platform');
     }
   }
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return '<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>';
+      return "ca-app-pub-3940256099942544/1033173712";
     } else if (Platform.isIOS) {
-      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
+      return "ca-app-pub-3940256099942544/4411468910";
     } else {
-      throw UnsupportedError('Unsupported platform');
+      throw new UnsupportedError("Unsupported platform");
     }
   }
 
   static String get rewardedAdUnitId {
     if (Platform.isAndroid) {
-      return '<YOUR_ANDROID_REWARDED_AD_UNIT_ID>';
+      return "ca-app-pub-3940256099942544/5224354917";
     } else if (Platform.isIOS) {
-      return '<YOUR_IOS_REWARDED_AD_UNIT_ID>';
+      return "ca-app-pub-3940256099942544/1712485313";
     } else {
-      throw UnsupportedError('Unsupported platform');
+      throw new UnsupportedError("Unsupported platform");
     }
   }
 }
