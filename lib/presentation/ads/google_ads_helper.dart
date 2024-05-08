e5da933694208c1306ae6beaad63c56935a38351
kimhyungmin <jeen14rla@gmail.com>
2024-05-08 15:56:08 +0900

    [ADD] add google ads helper class

---
+++ lib/presentation/ads/google_ads_helper.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+class GoogleAdsHelper {
+
+  static String get bannerAdUnitId {
+    if (Platform.isAndroid) {
+      return '<YOUR_ANDROID_BANNER_AD_UNIT_ID>';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_BANNER_AD_UNIT_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get interstitialAdUnitId {
+    if (Platform.isAndroid) {
+      return '<YOUR_ANDROID_INTERSTITIAL_AD_UNIT_ID>';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+
+  static String get rewardedAdUnitId {
+    if (Platform.isAndroid) {
+      return '<YOUR_ANDROID_REWARDED_AD_UNIT_ID>';
+    } else if (Platform.isIOS) {
+      return '<YOUR_IOS_REWARDED_AD_UNIT_ID>';
+    } else {
+      throw UnsupportedError('Unsupported platform');
+    }
+  }
+}
