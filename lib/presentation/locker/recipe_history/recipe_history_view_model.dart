a00ef8ee629b8bbc5de9ef6ef6cbfe3724e06f0a
leepl <yplunap25@gmail.com>
2024-05-09 15:36:32 +0900

    [ADD] history viewmodel get data for hive 기능 구현

--- lib/presentation/locker/recipe_history/recipe_history_view_model.dart
+++ lib/presentation/locker/recipe_history/recipe_history_view_model.dart
@@ -1,14 +1,31 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_project_orm_janggo/presentation/locker/recipe_history/recipe_history_state.dart';
+import 'package:hive/hive.dart';
+
+import '../../../data/db/history/history_recipe_data.dart';
 
 class RecipeHistoryViewModel with ChangeNotifier {
-  List<Container> _listTiles = [Container()];
+  RecipeHistoryState _state = const RecipeHistoryState();
 
-  List<Container> get listTiles => _listTiles;
+  RecipeHistoryState get state => _state;
 
-  void removeList(int index) {
-    if (_listTiles.length > 1) {
-      _listTiles.removeAt(index);
-      notifyListeners(); // 상태 변경을 UI에 알림
+  Future<void> getDataListFromHive() async {
+    var box = await Hive.openBox<HistoryRecipeData>('historyRecipeBox');
+    List<HistoryRecipeData> recipeList = box.values.toList();
+    List<int> ids = [];
+    List<String> imagePaths = [];
+    List<String> recipes = [];
+
+    if (recipeList != []) {
+      for (var recipeData in recipeList) {
+        ids.add(recipeData.id);
+        imagePaths.add(recipeData.imagePath);
+        recipes.add(recipeData.recipe);
+      }
     }
+
+    _state = _state.copyWith(id: ids, url: imagePaths, recipe: recipes);
+
+    notifyListeners();
   }
 }
