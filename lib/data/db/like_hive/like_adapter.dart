8b54574a738ad704ce527087c1a4fd6b903835a1
hee462 <a01080809887@gmail.com>
2024-05-09 19:50:23 +0900

    [ADD] TypeAdapter 생성

--- lib/data/db/like_hive/like_adapter.dart
+++ lib/data/db/like_hive/like_adapter.dart
@@ -3,42 +3,65 @@ import 'package:path_provider/path_provider.dart';
 
 import 'like_item.dart';
 
-class LikeBoxAdapter {
-  late Box<LikeItem> likeBox;
+class LikeBoxAdapter extends TypeAdapter<LikeItem> {
+  @override
+  LikeItem read(BinaryReader reader) {
+    return LikeItem(
 
-  Future<void> initialize() async {
-    final appDocDirectoryPath = await getApplicationDocumentsDirectory();
-    Hive.init(appDocDirectoryPath.path);
-    Hive.registerAdapter<LikeItem>(LikeItemAdapter());
-    likeBox = await Hive.openBox<LikeItem>('likeBox');
+        recipe: reader.readString(),
+        id: reader.readString(),
+        imageUrl: reader.readString(),
+        isLiked: reader.readBool());
   }
 
-  Future<void> addItem(LikeItem item) async {
-    try {
-      await likeBox.add(item);
-    } catch (e) {
-      print('Failed to add item: $e');
-    }
+  @override
+  // TODO: implement typeId
+  int get typeId => throw UnimplementedError();
+
+  @override
+  void write(BinaryWriter writer, LikeItem obj) {
+
+    writer.writeString(obj.recipe);
+    writer.writeString(obj.id);
+    writer.writeString(obj.imageUrl);
+    writer.writeBool(obj.isLiked);
   }
 
-  Future<void> removeItem(int index) async {
-    try {
-      await likeBox.deleteAt(index);
-    } catch (e) {
-      print('Failed to remove item at index $index: $e');
-    }
-  }
-
-  List<LikeItem> getItems() {
-    return likeBox.values.toList();
-  }
-
-  Future<void> dispose() async {
-    try {
-      await likeBox.close();
-      await Hive.close();
-    } catch (e) {
-      print('Failed to dispose: $e');
-    }
-  }
+//   late Box<LikeItem> likeBox;
+// // 데이터 담을 준비
+//   Future<void> initialize() async {
+//     final appDocDirectoryPath = await getApplicationDocumentsDirectory();
+//     Hive.init(appDocDirectoryPath.path);
+//     Hive.registerAdapter<LikeItem>(LikeItemAdapter());
+//     likeBox = await Hive.openBox<LikeItem>('likeBox');
+//   }
+//
+//   Future<void> addItem(LikeItem item) async {
+//     try {
+//       await likeBox.add(item);
+//     } catch (e) {
+//       print('아이템 추가 실패: $e');
+//     }
+//   }
+//
+//   Future<void> removeItem(int index) async {
+//     try {
+//       await likeBox.deleteAt(index);
+//     } catch (e) {
+//       print('아이템 삭제 실패 $index: $e');
+//     }
+//   }
+//
+//   Future<List<LikeItem>> getItems() async {
+//     return likeBox.values.toList();
+//   }
+//
+//   Future<void> dispose() async {
+//     try {
+//       await likeBox.close();
+//       await Hive.close();
+//     } catch (e) {
+//       print('dispose 실패: $e');
+//     }
+//   }
 }
