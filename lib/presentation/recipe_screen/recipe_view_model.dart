2044137c6353146a8d92e32649c185eefdc1d7b2
hee462 <a01080809887@gmail.com>
2024-05-09 19:53:35 +0900

    [ADD]  likeScreen  기능추가

--- lib/presentation/recipe_screen/recipe_view_model.dart
+++ lib/presentation/recipe_screen/recipe_view_model.dart
@@ -1,22 +1,33 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/get_picture_use_case.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/get_recipe_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/like_recipe_use_case/like_add_recipe_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/like_recipe_use_case/like_remove_recipe_use_case.dart';
+import 'package:flutter_project_orm_janggo/domain/use_case/like_recipe_use_case/like_search_recipe_use_case.dart';
 import 'package:flutter_project_orm_janggo/presentation/recipe_screen/recipe_state.dart';
+
+import '../../data/db/like_hive/like_adapter.dart';
+import '../../data/db/like_hive/like_item.dart';
+import '../../domain/use_case/like_recipe_use_case/like_load_recipe_use_case.dart';
 
 class RecipeViewModel with ChangeNotifier {
   final GetPictureUseCase _getPictureUseCase;
   final GetRecipeUseCase _getRecipeUseCase;
-  bool isLike = false;
+  final LikeAddRecipeUseCase _likeAddRecipeUseCase;
+  final LikeLoadRecipeUseCase _likeLoadRecipeUseCase;
+  final LikeRemoveRecipeUseCase _likeRemoveRecipeUseCase;
+  final LikeSearchRecipeUseCase _likeSearchRecipeUseCase;
+  final LikeBoxAdapter _likeBoxAdapter;
 
-  RecipeViewModel({
-    required GetPictureUseCase getPictureUseCase,
-    required GetRecipeUseCase getRecipeUseCase,
-  })  : _getPictureUseCase = getPictureUseCase,
-        _getRecipeUseCase = getRecipeUseCase;
+
+  List<LikeItem?> _likeItems = [];
+
 
   RecipeState _state = const RecipeState();
 
   RecipeState get state => _state;
+
+  List<LikeItem?> get likeItems => _likeItems;
 
 
   void getPicture(List<String> query) async {
@@ -38,6 +49,7 @@ class RecipeViewModel with ChangeNotifier {
       }
 
       _state = _state.copyWith(url: images);
+
       print(_state.url);
       notifyListeners();
     } catch (e) {
@@ -51,4 +63,50 @@ class RecipeViewModel with ChangeNotifier {
     notifyListeners();
   }
 
+  void search(String query) async {
+    _likeItems = await _likeSearchRecipeUseCase.execute(query);
+    notifyListeners();
+  }
+
+  void loadItem(LikeItem? item) {
+    _likeItems = _likeLoadRecipeUseCase.execute(item!) as List<LikeItem?>;
+    notifyListeners();
+  }
+
+
+  Future<void> addLikeItem(LikeItem item) async {
+    await _likeAddRecipeUseCase.execute(item);
+    loadItem(item);
+    _state = _state.copyWith(isLike: true);
+    notifyListeners();
+  }
+
+  Future<void> removeLikeItem(LikeItem item) async {
+    await _likeRemoveRecipeUseCase.execute(item);
+    loadItem(item);
+    _state = _state.copyWith(isLike: false);
+    notifyListeners();
+  }
+
+
+
+  RecipeViewModel({
+    required GetPictureUseCase getPictureUseCase,
+    required GetRecipeUseCase getRecipeUseCase,
+    required LikeAddRecipeUseCase likeAddRecipeUseCase,
+    required LikeLoadRecipeUseCase likeLoadRecipeUseCase,
+    required LikeRemoveRecipeUseCase likeRemoveRecipeUseCase,
+    required LikeSearchRecipeUseCase likeSearchRecipeUseCase,
+    required LikeBoxAdapter likeBoxAdapter,
+  })
+      : _getPictureUseCase = getPictureUseCase,
+        _getRecipeUseCase = getRecipeUseCase,
+        _likeAddRecipeUseCase = likeAddRecipeUseCase,
+        _likeLoadRecipeUseCase = likeLoadRecipeUseCase,
+        _likeRemoveRecipeUseCase = likeRemoveRecipeUseCase,
+        _likeSearchRecipeUseCase = likeSearchRecipeUseCase,
+        _likeBoxAdapter = likeBoxAdapter;
+
 }
+
+
