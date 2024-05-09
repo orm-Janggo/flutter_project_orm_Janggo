94e0d8be559b702b7a31d05755748e152ededf28
hee462 <a01080809887@gmail.com>
2024-05-09 11:51:50 +0900

    [ADD] 검색기능추가

--- lib/presentation/locker/recipe_like/recipe_like_view_model.dart
+++ lib/presentation/locker/recipe_like/recipe_like_view_model.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_project_orm_janggo/data/db/like_hive/like_hive_service.dart';
 
+import '../../../data/db/like_hive/like_adapter.dart';
 import '../../../data/db/like_hive/like_item.dart';
 
-class RecipeLikeViewModel with ChangeNotifier{
-   final LikeBox _likeBox;
+class RecipeLikeViewModel with ChangeNotifier {
+  final LikeBoxAdapter _likeBoxAdapter;
 
-   RecipeLikeViewModel({
-    required LikeBox likeBox,
-  }) : _likeBox = likeBox;
+  List<LikeItem> _likeItems = [];
+  List<LikeItem> _filteredLikeItems = [];
 
-   List<LikeItem> _likeItems = [];
+  List<LikeItem> get likeItems => _likeItems;
 
-   List<LikeItem> get likeItems => _likeItems;
+  List<LikeItem> get filteredLikeItems => _filteredLikeItems;
 
-   Future<void> addLikeItem(LikeItem item) async{
-     await _likeBox.addLikeItem(item);
-     _likeItems.add(item);
-     notifyListeners();
-   }
+  void search(String query) {
+    _filteredLikeItems = _likeItems
+        .where(
+            (e) => e.title.toLowerCase().trim().contains(query.toLowerCase()))
+        .toList();
+    notifyListeners();
+  }
 
-   Future<void> loadLikeItem() async{
-     _likeItems = await _likeBox.getLikeItems();
-     notifyListeners();
-   }
+  Future<void> addLikeItem(LikeItem item) async {
+    await _likeBoxAdapter.addItem(item);
+    _likeItems.add(item);
+    notifyListeners();
+  }
+
+  Future<void> removeLikeItem(int index) async {
+    await _likeBoxAdapter.removeItem(index);
+    _likeItems.removeAt(index);
+    notifyListeners();
+  }
+
+  Future<void> loadLikeItem() async {
+    _likeItems = await _likeBoxAdapter.getItems();
+    notifyListeners();
+  }
+
+  RecipeLikeViewModel({
+    required LikeBoxAdapter likeBoxAdapter,
+  }) : _likeBoxAdapter = likeBoxAdapter;
 }
