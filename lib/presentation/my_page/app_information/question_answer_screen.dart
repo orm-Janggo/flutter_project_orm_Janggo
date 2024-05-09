5173fb5a0d5743f79757671716ae47f9b5eed064
jiwon Choi <jiwon31273@naver.com>
2024-05-09 17:20:13 +0900

    [FIX] question_answer_screen.dart add singleChildScrollView

--- lib/presentation/my_page/app_information/question_answer_screen.dart
+++ lib/presentation/my_page/app_information/question_answer_screen.dart
@@ -7,48 +7,50 @@ class QuestionAnswerScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
-            child: const Text(
-              'Q&A',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 32.0, horizontal: 16.0),
+              child: const Text(
+                'Q&A',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
             ),
-          ),
-          Qna(
-            '레시피는 어떻게 검색하나요?',
-            '메인 화면에서 재료를 마음껏 작성하세요. 그리고 레시피 확인을 누르면 레시피를 추천해드립니다!',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Qna(
-            '비밀번호를 잃어버리셨나요?',
-            '로그인 화면에서 forgot password를 클릭해서 비밀번호를 찾으세요!',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Qna(
-            '이미지가 안나와요.',
-            '이미지는 외국 이미지 검색 Api를 사용하기 때문에 가끔 우리나라 음식에 대한 사진을 못가져올 때가 있어요 ㅠ',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          Qna(
-            '제가 검색한 레시피들은 어디서 보나요?',
-            '마이페이지에서 레시피 검색 기록 버튼을 누르시면 좋아요 기록까지 볼 수 있습니다!',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-        ],
+            Qna(
+              '레시피는 어떻게 검색하나요?',
+              '메인 화면에서 재료를 마음껏 작성하세요. 그리고 레시피 확인을 누르면 레시피를 추천해드립니다!',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Qna(
+              '비밀번호를 잃어버리셨나요?',
+              '로그인 화면에서 forgot password를 클릭해서 비밀번호를 찾으세요!',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Qna(
+              '이미지가 안나와요.',
+              '이미지는 외국 이미지 검색 Api를 사용하기 때문에 가끔 우리나라 음식에 대한 사진을 못가져올 때가 있어요 ㅠ',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            Qna(
+              '제가 검색한 레시피들은 어디서 보나요?',
+              '마이페이지에서 레시피 검색 기록 버튼을 누르시면 좋아요 기록까지 볼 수 있습니다!',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
       ),
     );
   }
