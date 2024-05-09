83d00f87c6b6b947fa6e49ac91c1ce7bbeb797a9
hee462 <a01080809887@gmail.com>
2024-05-09 11:52:20 +0900

    [ADD] 형변환 추가

--- lib/data/db/like_hive/like_hive_service.dart
+++ lib/data/db/like_hive/like_hive_service.dart
@@ -1,16 +1,16 @@
 import 'package:hive/hive.dart';
 
 import 'like_item.dart';
+class LikeBox {
+  static String _boxName = 'likebox';
 
-class LikeBox{
-  static String _boxName ='likebox';
-  Future<void> addLikeItem(LikeItem item) async{
+  Future<void> addLikeItem(LikeItem item) async {
     final box = await Hive.openBox<LikeItem>(_boxName);
     await box.add(item);
   }
 
-  Future<List<LikeItem>> getLikeItems() async{
+  Future<List<LikeItem>> getLikeItems() async {
     final likebox = await Hive.openBox<LikeItem>(_boxName);
-    return likebox.values.toList();
+    return likebox.values.map((dynamic item) => item as LikeItem).toList();
   }
 }
