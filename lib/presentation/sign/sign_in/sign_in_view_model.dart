549cc0f461f07e268ed34e0e0e1f2bc14ce0c44a
jiwon Choi <jiwon31273@naver.com>
2024-05-09 17:19:41 +0900

    [FIX] sign_in_view_model.dart add getUserInformation

--- lib/presentation/sign/sign_in/sign_in_view_model.dart
+++ lib/presentation/sign/sign_in/sign_in_view_model.dart
@@ -2,18 +2,36 @@ import 'package:flutter/material.dart';
 import 'package:flutter_project_orm_janggo/domain/use_case/firebase_auth_use_case/sign_in_with_email_password_use_case.dart';
 
 import '../../../data/user_information/user_information.dart';
+import '../../../domain/model/user_info_model/user_info_model.dart';
+import '../../../domain/use_case/firebase_auth_use_case/auth_state_changes_use_case.dart';
 
 class SignInViewModel with ChangeNotifier {
   final SignInWithEmailPasswordUseCase _signInWithEmailPasswordUseCase;
+  final AuthStateChangesUseCase _authStateChangesUseCase;
 
-  SignInViewModel(
-      {required SignInWithEmailPasswordUseCase signInWithEmailPasswordUseCase})
-      : _signInWithEmailPasswordUseCase = signInWithEmailPasswordUseCase;
+  SignInViewModel({
+    required SignInWithEmailPasswordUseCase signInWithEmailPasswordUseCase,
+    required AuthStateChangesUseCase authStateChangesUseCase,
+  })  : _signInWithEmailPasswordUseCase = signInWithEmailPasswordUseCase,
+        _authStateChangesUseCase = authStateChangesUseCase;
+
+  UserInfoModel? _firebaseUser;
+
+  UserInfoModel? get firebaseUser => _firebaseUser;
 
   Future<void> signInWithFirebaseAuth(String inputEmail, String inputPassword) async {
     await _signInWithEmailPasswordUseCase.execute(inputEmail, inputPassword);
-    UserInformation().updateLoginMethod(LoginMethod.email);
-    UserInformation().updateUser(email: inputEmail);
+    getUserInformation();
+
     notifyListeners();
   }
+
+  void getUserInformation() async {
+    _firebaseUser = _authStateChangesUseCase.execute();
+    UserInformation().updateLoginMethod(LoginMethod.email);
+    UserInformation().updateUser(email: firebaseUser!.email, displayName: firebaseUser!.displayName);
+
+    notifyListeners();
+  }
+
 }
