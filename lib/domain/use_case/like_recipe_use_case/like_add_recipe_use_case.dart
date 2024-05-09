24aad3473b7829cacd52c4a740cdbc5b474d80df
hee462 <a01080809887@gmail.com>
2024-05-09 19:51:06 +0900

    [ADD] add recipe use case 생성

---
+++ lib/domain/use_case/like_recipe_use_case/like_add_recipe_use_case.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_project_orm_janggo/data/db/like_hive/like_item.dart';
+
+import '../../../data/repository/like_recipe_repository_impl.dart';
+
+class LikeAddRecipeUseCase {
+  final LikeRecipeRepositoryImpl _likeRecipeRepositoryImpl;
+
+  LikeAddRecipeUseCase({
+    required LikeRecipeRepositoryImpl likeRecipeRepositoryImpl,
+  }) : _likeRecipeRepositoryImpl = likeRecipeRepositoryImpl;
+
+  Future<void> execute(LikeItem item) async {
+    return await _likeRecipeRepositoryImpl.addItem(item);
+  }
+
+
+}
