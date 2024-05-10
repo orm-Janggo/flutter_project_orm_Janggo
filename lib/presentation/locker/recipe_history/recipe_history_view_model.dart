c9c0c48dda111965452e566d6a92584fad754b89
leepl <yplunap25@gmail.com>
2024-05-10 18:00:34 +0900

    [ADD] history recipe detail func 구현

--- lib/presentation/locker/recipe_history/recipe_history_view_model.dart
+++ lib/presentation/locker/recipe_history/recipe_history_view_model.dart
@@ -61,4 +61,31 @@ class RecipeHistoryViewModel with ChangeNotifier {
     notifyListeners();
   }
 
+  Future<void> getDetailDataListFromHive(int id) async {
+    var box = await Hive.openBox<HistoryRecipeData>('historyRecipeBox');
+    List<HistoryRecipeData> recipeList = box.values.toList();
+    List<int> ids = [];
+    List<String> imagePaths = [];
+    List<String> recipes = [];
+
+    if (recipeList != []) {
+      for (var recipeData in recipeList) {
+        ids.add(recipeData.id);
+        imagePaths.add(recipeData.imagePath);
+        List<String> sentences = recipeData.recipe.split('\n');
+        String recipeName = '';
+        if (sentences.first.contains('레시피')) {
+          recipeName = sentences[2];
+        } else {
+          recipeName = sentences.first;
+        }
+        recipes.add(recipeName);
+      }
+    }
+
+    _state = _state.copyWith(id: ids, url: imagePaths, recipe: recipes);
+
+    notifyListeners();
+  }
+
 }
