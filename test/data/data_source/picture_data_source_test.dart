588bca93e567c9c81e1062dcebefaaea16fcc45f
hee462 <a01080809887@gmail.com>
2024-05-10 14:17:11 +0900

    [MODIFY] 주석처리

--- test/data/data_source/picture_data_source_test.dart
+++ test/data/data_source/picture_data_source_test.dart
@@ -7,21 +7,21 @@ import 'package:flutter_project_orm_janggo/domain/use_case/get_picture_use_case.
 import 'package:flutter_project_orm_janggo/domain/use_case/get_recipe_use_case.dart';
 import 'package:flutter_project_orm_janggo/presentation/recipe_screen/recipe_view_model.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-void main() {
-  test('pixabay test', () async {
-    await dotenv.load(fileName: '.env');
-    final dataSource = PictureDataSource();
-    final recipe = RecipeViewModel(
-      getPictureUseCase: GetPictureUseCase(repository: PictureRepositoryImpl(pictureDataSource: dataSource)),
-      getRecipeUseCase: GetRecipeUseCase(
-        chatGptRepositoryImpl: ChatGptRepositoryImpl(
-          dataSource: GptDataSource(),
-        ),
-      ),
-    );
-    recipe.getPicture(['김치찌개']);
-    final state = recipe.state;
-    expect(state.url, isNotNull);
-  });
-}
+//
+// void main() {
+//   test('pixabay test', () async {
+//     await dotenv.load(fileName: '.env');
+//     final dataSource = PictureDataSource();
+//     final recipe = RecipeViewModel(
+//       getPictureUseCase: GetPictureUseCase(repository: PictureRepositoryImpl(pictureDataSource: dataSource)),
+//       getRecipeUseCase: GetRecipeUseCase(
+//         chatGptRepositoryImpl: ChatGptRepositoryImpl(
+//           dataSource: GptDataSource(),
+//         ),
+//       ),
+//     );
+//     recipe.getPicture(['김치찌개']);
+//     final state = recipe.state;
+//     expect(state.url, isNotNull);
+//   });
+// }
