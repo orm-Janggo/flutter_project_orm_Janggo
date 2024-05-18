57147c5376a17f2ea8e87889e4bdd6d6536c78b4
kimhyungmin <jeen14rla@gmail.com>
2024-05-18 11:21:38 +0900

    [CHORE] remove unnecessary variables

--- lib/presentation/main/main_screen_view_model.dart
+++ lib/presentation/main/main_screen_view_model.dart
@@ -9,21 +9,11 @@ class MainScreenViewModel with ChangeNotifier {
     required AuthStateChangesUseCase authStateChangesUseCase,
   }) : _authStateChangesUseCase = authStateChangesUseCase;
 
-  String? _userEmail;
-  String? _userDisplayName;
   UserInfoModel? _firebaseUser;
-
-  String? get userEmail => _userEmail;
-
-  String? get userDisplayName => _userDisplayName;
 
   UserInfoModel? get firebaseUser => _firebaseUser;
 
   void getCurrentUserInfo() {
     _firebaseUser = _authStateChangesUseCase.execute();
-
-    _userEmail = firebaseUser?.email;
-
-    _userDisplayName = firebaseUser?.displayName;
   }
 }
