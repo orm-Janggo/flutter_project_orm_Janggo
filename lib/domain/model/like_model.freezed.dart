f7d9a6d3aaef0c90443b702d90a14c0663c56ecd
jiwon Choi <jiwon31273@naver.com>
2024-05-20 16:58:44 +0900

    [FIX] lie_model freezed, g

---
+++ lib/domain/model/like_model.freezed.dart
@@ -0,0 +1,257 @@
+// coverage:ignore-file
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
+
+part of 'like_model.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+final _privateConstructorUsedError = UnsupportedError(
+    'It seems like you constructed your class using `MyClass._()`. This constructor is only meant to be used by freezed and you are not supposed to need it nor use it.\nPlease check the documentation here for more information: https://github.com/rrousselGit/freezed#adding-getters-and-methods-to-our-models');
+
+LikeModel _$LikeModelFromJson(Map<String, dynamic> json) {
+  return _LikeModel.fromJson(json);
+}
+
+/// @nodoc
+mixin _$LikeModel {
+  String get recipe => throw _privateConstructorUsedError;
+  String get id => throw _privateConstructorUsedError;
+  String get imageUrl => throw _privateConstructorUsedError;
+  bool get isLiked => throw _privateConstructorUsedError;
+  String get foodName => throw _privateConstructorUsedError;
+  DateTime get time => throw _privateConstructorUsedError;
+
+  Map<String, dynamic> toJson() => throw _privateConstructorUsedError;
+  @JsonKey(ignore: true)
+  $LikeModelCopyWith<LikeModel> get copyWith =>
+      throw _privateConstructorUsedError;
+}
+
+/// @nodoc
+abstract class $LikeModelCopyWith<$Res> {
+  factory $LikeModelCopyWith(LikeModel value, $Res Function(LikeModel) then) =
+      _$LikeModelCopyWithImpl<$Res, LikeModel>;
+  @useResult
+  $Res call(
+      {String recipe,
+      String id,
+      String imageUrl,
+      bool isLiked,
+      String foodName,
+      DateTime time});
+}
+
+/// @nodoc
+class _$LikeModelCopyWithImpl<$Res, $Val extends LikeModel>
+    implements $LikeModelCopyWith<$Res> {
+  _$LikeModelCopyWithImpl(this._value, this._then);
+
+  // ignore: unused_field
+  final $Val _value;
+  // ignore: unused_field
+  final $Res Function($Val) _then;
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? recipe = null,
+    Object? id = null,
+    Object? imageUrl = null,
+    Object? isLiked = null,
+    Object? foodName = null,
+    Object? time = null,
+  }) {
+    return _then(_value.copyWith(
+      recipe: null == recipe
+          ? _value.recipe
+          : recipe // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      isLiked: null == isLiked
+          ? _value.isLiked
+          : isLiked // ignore: cast_nullable_to_non_nullable
+              as bool,
+      foodName: null == foodName
+          ? _value.foodName
+          : foodName // ignore: cast_nullable_to_non_nullable
+              as String,
+      time: null == time
+          ? _value.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ) as $Val);
+  }
+}
+
+/// @nodoc
+abstract class _$$LikeModelImplCopyWith<$Res>
+    implements $LikeModelCopyWith<$Res> {
+  factory _$$LikeModelImplCopyWith(
+          _$LikeModelImpl value, $Res Function(_$LikeModelImpl) then) =
+      __$$LikeModelImplCopyWithImpl<$Res>;
+  @override
+  @useResult
+  $Res call(
+      {String recipe,
+      String id,
+      String imageUrl,
+      bool isLiked,
+      String foodName,
+      DateTime time});
+}
+
+/// @nodoc
+class __$$LikeModelImplCopyWithImpl<$Res>
+    extends _$LikeModelCopyWithImpl<$Res, _$LikeModelImpl>
+    implements _$$LikeModelImplCopyWith<$Res> {
+  __$$LikeModelImplCopyWithImpl(
+      _$LikeModelImpl _value, $Res Function(_$LikeModelImpl) _then)
+      : super(_value, _then);
+
+  @pragma('vm:prefer-inline')
+  @override
+  $Res call({
+    Object? recipe = null,
+    Object? id = null,
+    Object? imageUrl = null,
+    Object? isLiked = null,
+    Object? foodName = null,
+    Object? time = null,
+  }) {
+    return _then(_$LikeModelImpl(
+      recipe: null == recipe
+          ? _value.recipe
+          : recipe // ignore: cast_nullable_to_non_nullable
+              as String,
+      id: null == id
+          ? _value.id
+          : id // ignore: cast_nullable_to_non_nullable
+              as String,
+      imageUrl: null == imageUrl
+          ? _value.imageUrl
+          : imageUrl // ignore: cast_nullable_to_non_nullable
+              as String,
+      isLiked: null == isLiked
+          ? _value.isLiked
+          : isLiked // ignore: cast_nullable_to_non_nullable
+              as bool,
+      foodName: null == foodName
+          ? _value.foodName
+          : foodName // ignore: cast_nullable_to_non_nullable
+              as String,
+      time: null == time
+          ? _value.time
+          : time // ignore: cast_nullable_to_non_nullable
+              as DateTime,
+    ));
+  }
+}
+
+/// @nodoc
+@JsonSerializable()
+class _$LikeModelImpl implements _LikeModel {
+  const _$LikeModelImpl(
+      {required this.recipe,
+      required this.id,
+      required this.imageUrl,
+      required this.isLiked,
+      required this.foodName,
+      required this.time});
+
+  factory _$LikeModelImpl.fromJson(Map<String, dynamic> json) =>
+      _$$LikeModelImplFromJson(json);
+
+  @override
+  final String recipe;
+  @override
+  final String id;
+  @override
+  final String imageUrl;
+  @override
+  final bool isLiked;
+  @override
+  final String foodName;
+  @override
+  final DateTime time;
+
+  @override
+  String toString() {
+    return 'LikeModel(recipe: $recipe, id: $id, imageUrl: $imageUrl, isLiked: $isLiked, foodName: $foodName, time: $time)';
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return identical(this, other) ||
+        (other.runtimeType == runtimeType &&
+            other is _$LikeModelImpl &&
+            (identical(other.recipe, recipe) || other.recipe == recipe) &&
+            (identical(other.id, id) || other.id == id) &&
+            (identical(other.imageUrl, imageUrl) ||
+                other.imageUrl == imageUrl) &&
+            (identical(other.isLiked, isLiked) || other.isLiked == isLiked) &&
+            (identical(other.foodName, foodName) ||
+                other.foodName == foodName) &&
+            (identical(other.time, time) || other.time == time));
+  }
+
+  @JsonKey(ignore: true)
+  @override
+  int get hashCode =>
+      Object.hash(runtimeType, recipe, id, imageUrl, isLiked, foodName, time);
+
+  @JsonKey(ignore: true)
+  @override
+  @pragma('vm:prefer-inline')
+  _$$LikeModelImplCopyWith<_$LikeModelImpl> get copyWith =>
+      __$$LikeModelImplCopyWithImpl<_$LikeModelImpl>(this, _$identity);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return _$$LikeModelImplToJson(
+      this,
+    );
+  }
+}
+
+abstract class _LikeModel implements LikeModel {
+  const factory _LikeModel(
+      {required final String recipe,
+      required final String id,
+      required final String imageUrl,
+      required final bool isLiked,
+      required final String foodName,
+      required final DateTime time}) = _$LikeModelImpl;
+
+  factory _LikeModel.fromJson(Map<String, dynamic> json) =
+      _$LikeModelImpl.fromJson;
+
+  @override
+  String get recipe;
+  @override
+  String get id;
+  @override
+  String get imageUrl;
+  @override
+  bool get isLiked;
+  @override
+  String get foodName;
+  @override
+  DateTime get time;
+  @override
+  @JsonKey(ignore: true)
+  _$$LikeModelImplCopyWith<_$LikeModelImpl> get copyWith =>
+      throw _privateConstructorUsedError;
+}
